8cabb35f0ae5279422d48604b47cfa173f3701d6
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-02-06 01:44:24 +0000

    1

---
+++ engine.js
@@ -0,0 +1,17 @@
+function pigIt(str) {
+    //Code here
+    let arr = [];
+    let count = 0;
+    arr = str.split(' ');
+    console.log("splitted arr is: ", arr)
+    let index;
+    let r = [];
+    for (let i = 0; i < arr.length; i++) {
+        index = arr[i][0];
+        let s2 = arr[i].substring(1);
+        r[i] = s2 + index + "ay";
+        console.log(r[i])
+    }
+    let result = r.join(' ');
+    return result;
+}
